1593ab1f9bb356dcc204bb2a6d313c9143414206
Oli Forster <oliforster798@gmail.com>
2016-02-22 13:26:35 +0000

    updated user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222095518) do
+ActiveRecord::Schema.define(version: 20160222122638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160222095518) do
     t.string "full_name"
     t.string "email"
     t.date   "start_date"
-    t.string "type"
+    t.string "user_type"
     t.string "password_digest"
   end
 
