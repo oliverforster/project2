ab8bb8e7f6adcffa0748f4fd63cc07d74696b7c7
Oli Forster <oliforster798@gmail.com>
2016-02-23 15:44:58 +0000

    more views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,21 @@
+get '/recipes' do
+
+  erb :'recipes/index'
+end
+
+get 'recipes/:id' do
+
+
+  erb :'recipes/show'
+end
+
 get '/recipes/new' do
   erb :'recipes/new'
+end
+
+post '/recipes' do
+  @recipe = Recipe.new(params[:recipe])
+  if @recipe.save
+    redirect "/recipes"
+  end
 end
