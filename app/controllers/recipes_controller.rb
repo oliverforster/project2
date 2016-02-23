47a980d28feb193a2046234501d0a541b67355b5
Oli Forster <oliforster798@gmail.com>
2016-02-23 17:36:42 +0000

    fixed some stuff

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,19 +3,27 @@ get '/recipes' do
   erb :'recipes/index'
 end
 
-get 'recipes/:id' do
-  @recipe = Recipe.find(params[:id])
-  erb :'recipes/show'
-end
 
 get '/recipes/new' do
   @recipe = Recipe.new
   erb :'recipes/new'
 end
 
+
+
 post '/recipes' do
   @recipe = Recipe.new(params[:recipe])
   if @recipe.save
+    current_user.recipes << @recipe
     redirect "/recipes"
   end
 end
+
+get '/recipes/:id' do
+  @recipe = Recipe.find(params[:id])
+  erb :'recipes/show'
+end
+
+
+
+
