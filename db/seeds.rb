5577f009eaebc90a7ee1f8978cb1f5ffa94e8c04
Oli Forster <oliforster798@gmail.com>
2016-02-24 14:11:42 +0000

    fixing stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,22 +34,33 @@ u_oli = User.create(username: "Oli",
                     user_type: "chef",
                     password: "hello") 
 
-[
-  {
-    challenge_id: 1,
-    method: "chop cook serve",
-    ingredients: "flour eggs cucumber",
-    name:"cake2"
-  }].each do |recipe|
-  Recipe.create!(recipe)
-end
+u_max = User.create(username: "Maxxx",
+                    full_name: "Max Hilliard",
+                    email: "max@gmail.com",
+                    start_date: "2/21/2002",
+                    user_type: "chef",
+                    password: "hello") 
+
 
 r_cake1 = Recipe.create(challenge_id: 1,
                         method: "chop cook serve",
                         ingredients: "flour eggs tomato",
                         name:"cake1")
 
+r_cake2 = Recipe.create(challenge_id: 1,
+                        method: "chop cook serve",
+                        ingredients: "flour eggs cucumber",
+                        name:"cake2")
+
+r_cake3 = Recipe.create(challenge_id: 1,
+                        method: "chop cook serve",
+                        ingredients: "flour eggs potato",
+                        name:"potatocake")
+
 u_oli.recipes << r_cake1
+u_oli.recipes << r_cake2
+u_max.recipes << r_cake3
+
 
 [ {
   name: "cake challenge",
