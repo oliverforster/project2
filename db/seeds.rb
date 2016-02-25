caeb88cbf757b67576513dc22a3e34c4dc7fd4e4
Oli Forster <oliforster798@gmail.com>
2016-02-25 17:21:23 +0000

    updating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,8 @@ Challenge.destroy_all
 end
 
 [ {
-  name: "cake challenge",
-  information: "create a safe cake"
+  name: "Pasta Challenge",
+  information: "create a pasta dish"
   }].each do |challenge|
     Challenge.create!(challenge)
 end
@@ -55,15 +55,15 @@ u_max = User.create(username: "Maxxx",
 
 r_cake1 = Recipe.create(challenge_id: Challenge.first.id,
                         chef_id: u_oli.id,
-                        method: "chop cook serve",
-                        ingredients: "flour eggs tomato",
-                        name:"cake1")
+                        method: "Trim, halve and chop the leek into chunks, and peel the garlic.<br> Put a large pan of salted water on to boil, then drop in the leek and garlic cloves. Cook for 3 minutes, then add your cavolo nero, kale or cabbage. Cook for another 3 to 3½ minutes, but no more! Don’t be tempted.<br>Using tongs or a slotted spoon, move the veg to a liquidiser – it’s OK if a bit of water comes with it. Add your oil and liquidise until you’ve got a vivid green purée, the same consistency as double cream. If it’s a bit too dry, add a splash of the cooking water. Have a taste, season with sea salt and black pepper, then put aside.<br>Bring the water back to boil, and cook the pasta according to the packet instructions.<br>Drain and return the pasta to the pan, add the sauce and allow it to simmer over a medium-low heat for 30 seconds.<br>As it comes back to the boil, remove from the heat and finely grate in the Parmesan. Be quick, so your pasta doesn’t overcook, then check the balance of cheese, salt and pepper.<br>Finely grate over extra Parmesan, then serve on a big platter and eat as soon as possible.",
+                        ingredients: "1 large leek<br>6 cloves of garlic<br>2 large handfuls of cavolo nero, curly kale or savoy cabbage<br>100 ml extra virgin olive oil<br>500g dried pasta, such as penne, linguine or spaghetti<br>100g Parmesan cheese"
+                        name:"World’s most elegant winter pasta")
 
 r_cake2 = Recipe.create(challenge_id: Challenge.first.id,
                         chef_id: u_oli.id,
-                        method: "chop cook serve",
-                        ingredients: "flour eggs cucumber",
-                        name:"cake2")
+                        method: "If you are cooking this just before you eat, put the water for the penne on to boil before you start the sauce. You will need a big pan, enough to take the pasta and its sauce later.<br>Finely chop the onion, either by hand or in a processor. In a large pan, heat the garlic oil and add the finely chopped onion and a good sprinkling of salt. Cook the onion fairly gently for about 15 minutes without letting it catch and burn, which just means giving it a stir every now and again. It should be very soft and almost beginning to caramelize.<br>Tip in the cans of chopped tomatoes and continue cooking over a gentle heat, simmering for another 15-20 minutes. If you're cooking this ahead, and I always do, stop here.<br>Reheat the almost-finished tomatoes (or just continue as you were if you're making this in one unbroken fluid movement), stir in the double cream and take the pan off the heat. When the water for the pasta comes to the boil add a good measure of salt and tip in the penne. Set a timer for 3-4 minutes less than the packet instructions for cooking it, as you want to make sure it's cooked al dente and will need to start tasting early.<br>Drain the cooked pasta, tip it back in the pan and pour over the vodka, and add the butter and some more salt. Turn the penne in the vodka and melting butter and then tip it into the tomato sauce unless it is easier to pour the tomato sauce over the pasta: it depends on the sizes of pans you are using.<br>Toss the pasta in the sauce until it is evenly coated and turn out into a large, warmed bowl. Put it on the table along with a block of Parmesan and a grater.",
+                        ingredients: "1 large onion<br>2 tablespoons garlic infused olive oil<br>salt (for pasta water, to taste)<br>820 grams canned chopped tomatoes<br>2 tablespoons double cream<br>1 kilogram penne rigate (or other short preferably ridged pasta)<br>125 ml vodka<br>4 tablespoons unsalted butter<br>parmesan cheese (for grating over at the table)<br>",
+                        name:"Penne alla vodka")
 
 r_cake3 = Recipe.create(challenge_id: Challenge.first.id,
                         chef_id: u_max.id,
